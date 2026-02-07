426cdaaf44e529407a480402c8348b8def355fce
KatarinaZiakova <katkaziakova04@gmail.com>
2026-02-08 00:15:25 +0100

    upravena stranka s oblubenymi trasami

--- personalization/urls.py
+++ personalization/urls.py
@@ -6,4 +6,5 @@ app_name = 'personalization'
 urlpatterns = [
     path('favorites/', views.favorites, name='favorites'),
     path('add-favorite-ajax/', views.add_favorite_ajax, name='add_favorite_ajax'),
+    path('delete/<int:pk>/', views.delete_favorite, name='delete_favorite'),
 ]
--- personalization/views.py
+++ personalization/views.py
@@ -1,23 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import FavouriteRoute
-
+from data_management.models import Stop
+from django.contrib import messages
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 import json
 
 @login_required
 def favorites(request):
-    """Zobrazí obľúbené trasy používateľa"""
+    """Zobrazí obľúbené trasy používateľa s IDčkami pre linky"""
     favourite_routes = FavouriteRoute.objects.filter(user=request.user)
+    
+    enhanced_routes = []
+    for fav in favourite_routes:
+        start_stop = Stop.objects.filter(name=fav.startLocation).first()
+        end_stop = Stop.objects.filter(name=fav.endLocation).first()
+        
+        enhanced_routes.append({
+            'id': fav.id,
+            'start_name': fav.startLocation,
+            'end_name': fav.endLocation,
+            'start_id': start_stop.id if start_stop else None,
+            'end_id': end_stop.id if end_stop else None,
+        })
+
     context = {
-        'favourite_routes': favourite_routes
+        'favourite_routes': enhanced_routes
     }
     return render(request, 'personalization/favorites.html', context)
 
 @login_required
+def delete_favorite(request, pk):
+    """Vymazanie obľúbenej trasy"""
+    fav = get_object_or_404(FavouriteRoute, pk=pk, user=request.user)
+    if request.method == 'POST':
+        fav.delete()
+        messages.success(request, "Trasa bola odstránená z obľúbených.")
+    return redirect('personalization:favorites')
+
+@login_required
 @require_POST
 def add_favorite_ajax(request):
+    """Pridanie obľúbenej trasy cez AJAX"""
     try:
         data = json.loads(request.body)
         start_name = data.get('start')
